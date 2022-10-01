aa8f4bdb373ecce90a9e266f4d3c3fd629307777
rogeatic <roguedagger1@gmail.com>
2022-09-30 18:22:20 -0600

    ok fix

---
+++ mission/mission.js
@@ -0,0 +1,9 @@
+// const hList = [ 
+//     "Building testimonies of the restored gospel of Jesus Christ and fostering its principles in a wholesome academic, cultural, and social environment.",
+//     "Providing a high-quality education that prepares students of diverse interests and abilities for lifelong learning and employment.",
+//     "Serving as many students as possible within resource constraints.",
+//     "Delivering education that is affordable for students and the Church."
+// ]
+// const newList = document.createElement("ul")
+// newList.innerHTML = `<li>${hList[0]}</li><li>${hList[1]}</li><li>${hList[2]}</li><li>${hList[3]}</li>`
+// document.body.appendChild(newList);
