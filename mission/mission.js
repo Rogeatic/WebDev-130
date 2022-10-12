8d4715d31631f264432ff2daeb6ce24df21a6e73
rogeatic <roguedagger1@gmail.com>
2022-10-12 13:39:33 -0600

    dark theme

--- mission/mission.js
+++ mission/mission.js
@@ -7,3 +7,22 @@
 // const newList = document.createElement("ul")
 // newList.innerHTML = `<li>${hList[0]}</li><li>${hList[1]}</li><li>${hList[2]}</li><li>${hList[3]}</li>`
 // document.body.appendChild(newList);
+function changeTheme(){
+    val = document.querySelector("#theme_selector").selectedIndex;
+    console.log(val);
+
+    if(val == 1){ //Dark
+        if(!document.body.classList.contains("dark")){
+            document.body.classList.add("dark");
+            document.querySelector("footPic").src = "mission/byui-logo_white.png";
+        }
+    }
+    else{ //should be Light
+        if(document.body.classList.contains("dark")){
+            document.body.classList.remove("dark");
+            document.querySelector("footPic").src = "mission/byui-logo_blue.webp";
+        }
+    }
+}
+
+document.querySelector("#theme_selector").addEventListener("change", changeTheme)
