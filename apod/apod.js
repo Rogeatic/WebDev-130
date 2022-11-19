6aa88b5e917e7d8b76508a44152574d3e45f2d37
rogeatic <roguedagger1@gmail.com>
2022-11-19 14:59:14 -0700

    fixed

--- apod/apod.js
+++ apod/apod.js
@@ -13,28 +13,9 @@ async function getApod(url){
 async function output() {
   const data = await getApod(apodUrl)
   console.log(data);
-  document.body.querySelector("#images").insertAdjacentHTML("beforeend", `<p> Name: ${data.title}</p><img src="${data.url} id="SpacePic" alt="${data.explanation}">`);
+  document.body.querySelector("#images").insertAdjacentHTML("beforeend", `<p> Name: ${data.title}</p><img src="${data.url}" alt="${data.explanation}">`);
   
 }
 
 
 output();
-
-// async function getApod(url){
-    
-//     const response = await fetch(url);
-//     console.log(response);
-//     return (response);
-// }
-// function makePhoto(photo){
-//     document.getElementById("#images").insertAdjacentHTML( "beforeend",`<img src=${photo.url} alt="${photo.title}">
-//     <div>
-//         <h2>${photo.title}</h2>
-//         <h3>${photo.date}</h3>
-//         <p>${photo.explanation}</p>
-//     </div>`);
-// }
-// function init(){
-//     makePhoto(getApod(apodUrl))
-// }
-// init();
