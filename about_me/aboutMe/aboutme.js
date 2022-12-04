0721bc0e365e4e94ab5d3cd860ec7c5185ff2aae
rogeatic <roguedagger1@gmail.com>
2022-12-04 11:23:31 -0700

    cool

--- about_me/aboutMe/aboutme.js
+++ about_me/aboutMe/aboutme.js
@@ -2,8 +2,8 @@ console.log("It worked!");
 
 const FavoriteFoodsList = ["WaterMelon", "Pizza", "BBQ chips", "Icecream"];
 const indList = document.createElement('FoodList');
-indList.innerHTML = `<br><br><p1>Favorite Foods:</p1> <li>${FavoriteFoodsList[0]} </li>
-    <li>${FavoriteFoodsList[1]} </li> <li>${FavoriteFoodsList[2]} </li><li>${FavoriteFoodsList[3]} </li>`;
+indList.innerHTML = `<div id="foods"><br><br><p1>Favorite Foods:</p1> <li>${FavoriteFoodsList[0]} </li>
+    <li>${FavoriteFoodsList[1]} </li> <li>${FavoriteFoodsList[2]} </li><li>${FavoriteFoodsList[3]} </li></div>`;
 document.body.appendChild(indList);
 
 const today = new Date();
